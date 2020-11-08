f457158e4d10e43e6daafbbe6bc855dd0bfe24c1
daniphant <daniphant@protonmail.com>
2020-11-08 20:36:28 -0300

    fix router redeclaration

--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,11 @@ import * as UserController from "./controllers/UserController.ts";
 
 const router = new Router();
 
-router.get("/users/", UserController.indexUsers)
-router.post("/users/", UserController.storeUser)
-router.get("/users/:id", UserController.indexUser)
-router.put("/users/:id", UserController.updateUser)
-router.post("/users/:id/delete", UserController.deleteUser)
+router
+    .get("/users/", UserController.indexUsers)
+    .post("/users/", UserController.storeUser)
+    .get("/users/:id", UserController.indexUser)
+    .put("/users/:id", UserController.updateUser)
+    .post("/users/:id/delete", UserController.deleteUser)
 
 export default router;
